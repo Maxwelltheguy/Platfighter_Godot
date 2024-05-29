631da6f1c24c02e1815a9911cf4583ca28707a40
Maxwell <maxwellmoss332009@gmail.com>
2024-05-29 14:14:40 -0400

    Added Player Animations

--- Platformer Test/Scripts/character_body_2d.gd
+++ Platformer Test/Scripts/character_body_2d.gd
@@ -14,12 +14,13 @@ func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
-	
+		$AnimatedSprite2D.play("Fall")
 	
 	
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -31,6 +32,7 @@ func _physics_process(delta):
 				$AnimatedSprite2D.scale.x = -1
 			elif Input.is_action_pressed("right"):
 				$AnimatedSprite2D.scale.x = 1
+			$AnimatedSprite2D.play("Run")
 			
 		else:
 			velocity.x = direction * SPEED
@@ -38,9 +40,18 @@ func _physics_process(delta):
 				$AnimatedSprite2D.scale.x = -1
 			elif Input.is_action_pressed("right"):
 				$AnimatedSprite2D.scale.x = 1
+			$AnimatedSprite2D.play("Walk")
 		
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+		$AnimatedSprite2D.play("Idle")
+	if not is_on_floor():
+		if velocity.y <= 0:
+			$AnimatedSprite2D.play("Jump")
+		else:
+			$AnimatedSprite2D.play("Fall")
+	
+	
 
 
 	move_and_slide()
